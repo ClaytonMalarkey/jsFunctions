6f0ae2c09fd9e5bc58bb603ae1928e9783c40f90
ClaytonMalarkey <malarkey1985@gmail.com>
2023-09-22 19:42:56 -0600

    second commit

---
+++ JsDomCheck/js/app.js
@@ -0,0 +1,6 @@
+let all_p_tags = document.querySelectorAll('p');
+console.log(all_p_tags);
+let all_ids_of_pinhead = document.getElementById('pinhead');
+console.log(all_ids_of_pinhead);
+let all_classes_of_header2inhead = document.getElementsByClassName('h2inhead');
+console.log(all_classes_of_header2inhead)
